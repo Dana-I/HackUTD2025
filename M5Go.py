2fb92ae0396cf979daddd95c31fe1672838794c3
Jazmin Gutierrez <jazminguti06@gmail.com>
2025-11-08 22:21:53 -0600

    adjust text layout

--- M5Go.py
+++ M5Go.py
@@ -13,16 +13,16 @@ random2 = None
 i = None
 
 
-label0 = M5TextBox(30, 104, "Temperature (F) :", lcd.FONT_Default, 0xFFFFFF, rotate=0)
-label1 = M5TextBox(30, 144, "Pressure (hPa) :", lcd.FONT_Default, 0xFFFFFF, rotate=0)
-label2 = M5TextBox(30, 185, "Humidity (%) :", lcd.FONT_Default, 0xFFFFFF, rotate=0)
-label8 = M5TextBox(30, 220, "Sound (db) :", lcd.FONT_Default, 0xFFFFFF, rotate=0)
-label3 = M5TextBox(210, 104, "Text", lcd.FONT_Default, 0xffffff, rotate=0)
+label0 = M5TextBox(30, 120, "Temperature (F) :", lcd.FONT_Default, 0xFFFFFF, rotate=0)
+label1 = M5TextBox(30, 150, "Pressure (hPa) :", lcd.FONT_Default, 0xFFFFFF, rotate=0)
+label2 = M5TextBox(30, 180, "Humidity (%) :", lcd.FONT_Default, 0xFFFFFF, rotate=0)
+label8 = M5TextBox(30, 210, "Sound (db) :", lcd.FONT_Default, 0xFFFFFF, rotate=0)
+label3 = M5TextBox(210, 120, "Text", lcd.FONT_Default, 0xffffff, rotate=0)
 title0 = M5Title(title="UNIT ENV III", x=120, fgcolor=0xFFFFFF, bgcolor=0xff0000)
-label4 = M5TextBox(210, 144, "Text", lcd.FONT_Default, 0xFFFFFF, rotate=0)
-label5 = M5TextBox(210, 184, "Text", lcd.FONT_Default, 0xFFFFFF, rotate=0)
-label6 = M5TextBox(20, 50, "Status : ", lcd.FONT_DejaVu24, 0xffffff, rotate=0)
-label7 = M5TextBox(60, 50, " ", lcd.FONT_DejaVu24, 0x8B0000, rotate=0)
+label4 = M5TextBox(210, 150, "Text", lcd.FONT_Default, 0xFFFFFF, rotate=0)
+label5 = M5TextBox(210, 180, "Text", lcd.FONT_Default, 0xFFFFFF, rotate=0)
+label6 = M5TextBox(10, 50, "Status : ", lcd.FONT_DejaVu24, 0xffffff, rotate=0)
+label7 = M5TextBox(120, 50, " ", lcd.FONT_DejaVu24, 0x8B0000, rotate=0)
 
 
 while True:
@@ -32,15 +32,13 @@ while True:
   label4.setText(str(env3_0.pressure))
   label5.setText(str(env3_0.humidity))
   
-  if (tempF > 72):
+  if (tempF > 85):
     label7.setText("Temp too high!")
     label7.setColor(0x8B0000)
   else:
-    label17.setText("Temp OK")
+    label7.setText("Normal stats")
     label7.setColor(0xffffff)
     
-  mic_value = adc.read()  # Get analog sound level
-  db = 20 * math.log10(mic_value / reference_value)
   
   wait(0.1)
   wait_ms(2)
